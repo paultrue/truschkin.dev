07ab0be7038e0c826a7d8b897659585fb4577c46
paultrue <paul@truschkin.com>
2020-04-11 11:17:16 +0200

    add gulp to enable sass

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const cssnano = require("gulp-cssnano");
+
+gulp.task("css", function() {
+  return gulp
+    .src("./src/assets/scss/main.scss")
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(cssnano())
+    .on("error", sass.logError)
+    .pipe(gulp.dest("./dist/assets/css"));
+});
+
+gulp.task("watch", function() {
+  gulp.watch("./assets/scss/**/*.scss", gulp.parallel("css"));
+});
+
+gulp.task("build", gulp.parallel("css"));
